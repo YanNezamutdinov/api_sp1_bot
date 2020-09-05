548aa65a5eb83c3edd827f9c27dbfc83a8a34226
YanNezamutdinov <nezamutdinovyan@gmail.com>
2020-09-05 16:25:49 +0300

    New

--- homework.py
+++ homework.py
@@ -10,11 +10,13 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRACTICUM_URI = os.getenv('PRACTICUM_URI')
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework.get('homework_name')
+    if homework.get('status') == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,18 +24,19 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    params = {'from_date': current_timestamp}
+    homework_statuses = requests.get(PRACTICUM_URI, params=params, headers=headers)
     return homework_statuses.json()
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    return bot.send_message(chat_id = CHAT_ID, text = message)
 
 
 def main():
     current_timestamp = int(time.time())  # начальное значение timestamp
+    bot.send_message(chat_id = CHAT_ID, text = f'praktikum-status запущен! {current_timestamp}')
 
     while True:
         try:
