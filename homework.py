e6c4ae5daff48a7f3d06e54e172993d554a867bb
YanNezamutdinov <nezamutdinovyan@gmail.com>
2020-09-09 15:03:06 +0300

    Add try

--- homework.py
+++ homework.py
@@ -10,34 +10,39 @@ load_dotenv()
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-#PRACTICUM_URI = os.getenv('PRACTICUM_URI')
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
+
 def parse_homework_status(homework):
-    homework_name = homework.get('homework_name')
-    if homework.get('status') == 'rejected':
-        verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    if homework.get('homework_name'):
+        homework_name = homework.get('homework_name')
+        if homework.get('status') == 'rejected':
+            verdict = 'К сожалению в работе нашлись ошибки.'
+        else:
+            verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    return "Oops! homework_name not found!"
 
 
 def get_homework_statuses(current_timestamp):
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/' # fix
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     params = {'from_date': current_timestamp}
-    homework_statuses = requests.get(url, params=params, headers=headers)
+    try:
+        homework_statuses = requests.get(url, params=params, headers=headers)
+    except Exception as e:
+        return print(f'Oops! Error: {e}')
     return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(chat_id = CHAT_ID, text = message)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
     current_timestamp = int(time.time())  # начальное значение timestamp
-    bot.send_message(chat_id = CHAT_ID, text = f'praktikum-status запущен! {current_timestamp}')
+    bot.send_message(chat_id=CHAT_ID, text=f'praktikum-status запущен! {current_timestamp}')
 
     while True:
         try:
